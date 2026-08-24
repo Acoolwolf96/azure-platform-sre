bdeb36f97fabae9aaeecf5fec2105b8c837c1be1
Acoolwolf96 <nlsnnjoku@gmail.com>
2026-08-24 15:55:11 +0000

    Add Azure network foundation

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,24 @@
 resource "azurerm_resource_group" "platform" {
   name     = "rg-platform-sre"
-  location = "westeurope"
+  location = "northeurope"
+}
+
+resource "azurerm_virtual_network" "platform" {
+  name                = "vnet-platform-sre"
+  address_space       = ["10.50.0.0/16"]
+  location            = azurerm_resource_group.platform.location
+  resource_group_name = azurerm_resource_group.platform.name
 
   tags = {
     project     = "azure-platform-sre"
     environment = "local"
     managed_by  = "terraform"
   }
+}
+
+resource "azurerm_subnet" "aks" {
+  name                 = "snet-aks"
+  resource_group_name  = azurerm_resource_group.platform.name
+  virtual_network_name = azurerm_virtual_network.platform.name
+  address_prefixes     = ["10.50.1.0/24"]
 }
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,3 +5,11 @@ output "resource_group_name" {
 output "resource_group_id" {
   value = azurerm_resource_group.platform.id
 }
+
+output "virtual_network_name" {
+  value = azurerm_virtual_network.platform.name
+}
+
+output "aks_subnet_id" {
+  value = azurerm_subnet.aks.id
+}
